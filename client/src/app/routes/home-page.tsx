447629ed37a049b1c726233ba0d78a3665e21219
Jaafar Fora <jaafarfora@gmail.com>
2023-06-15 15:11:31 +0200

    front: test new environment

--- client/src/app/routes/home-page.tsx
+++ client/src/app/routes/home-page.tsx
@@ -7,7 +7,6 @@ import retweetIcon from '../../images/repost.png';
 import likeHeart from '../../images/heart.png';
 import CountUp from 'react-countup';
 
-
 const HomePage = () => {
   const navigate = useNavigate();
 
@@ -15,11 +14,10 @@ const HomePage = () => {
     navigate('/signup');
   };
 
-
   return (
     <div className="home-page">
       <Header />
-      <h1 className="slogan">Tweeting made simple</h1>
+      <h1 className="slogan">Tweeting made simple!</h1>
       <div className="home-container">
         <div className="banner">
           <div className="banner-icon-slogan">
@@ -49,39 +47,53 @@ const HomePage = () => {
 
         <div className="tweet-counter-container">
           <div className="counter-container">
-            <img src={gif} alt="" className='up-icon' />
+            <img src={gif} alt="" className="up-icon" />
             <h2>Followers</h2>
-            <CountUp end={10000} duration={20}/>
+            <CountUp end={10000} duration={20} />
           </div>
 
-          <div className='tweet-wrap-homepage'>
-            <div className='tweet-header'>
+          <div className="tweet-wrap-homepage">
+            <div className="tweet-header">
               <img src={userIcon} alt="" className="avator" />
               <div className="tweet-header-info">
-                <p className='user-name'>Maxwell Blaze</p>
-                <span className='user-screen-name'>@MaxwellBlaze</span>
-                <p className='tweet-text'>Everything turns so easy with @PinkHawk!<br/> Highly recommended if you want to post on twitter with automation tool and to manage your followers easily!</p>
+                <p className="user-name">Maxwell Blaze</p>
+                <span className="user-screen-name">@MaxwellBlaze</span>
+                <p className="tweet-text">
+                  Everything turns so easy with @PinkHawk!
+                  <br /> Highly recommended if you want to post on twitter with
+                  automation tool and to manage your followers easily!
+                </p>
 
                 <div className="tweet-info-counts">
                   <div className="comments">
-                    <img height="3" className="tweet-icon" src={commentIcon} alt="" />
+                    <img
+                      height="3"
+                      className="tweet-icon"
+                      src={commentIcon}
+                      alt=""
+                    />
                     <div className="comment-count">1435</div>
                   </div>
-                  <div className='retweets'>
-                    <img className='tweet-icon' src={retweetIcon} alt='retweetIcon' />
-                    <div className='retweet-count'>586</div>
+                  <div className="retweets">
+                    <img
+                      className="tweet-icon"
+                      src={retweetIcon}
+                      alt="retweetIcon"
+                    />
+                    <div className="retweet-count">586</div>
                   </div>
-                  <div className='likes'>
-                    <img className='tweet-icon' src={likeHeart} alt='likeHeart' />
-                    <div className='likes-count'>
-                      2.3k
-                    </div>
+                  <div className="likes">
+                    <img
+                      className="tweet-icon"
+                      src={likeHeart}
+                      alt="likeHeart"
+                    />
+                    <div className="likes-count">2.3k</div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
       </div>
       <Outlet />
