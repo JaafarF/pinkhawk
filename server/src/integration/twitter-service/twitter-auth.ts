2a6a2c4f529b65b1e8b8f2e9e2bef70ae4d067b9
Jaafar Fora <jaafarfora@gmail.com>
2023-06-19 15:04:05 +0200

    debug twitter stats

--- server/src/integration/twitter-service/twitter-auth.ts
+++ server/src/integration/twitter-service/twitter-auth.ts
@@ -85,29 +85,29 @@ export const getAccessToken = async (req: Request, res: Response) => {
   });
 
   //get user followers and likes
-  const followers = await realUser.v2.followers(user?.twitterAccountId!);
-  const followersCount = followers.meta.result_count;
+  // const followers = await realUser.v2.followers(user?.twitterAccountId!);
+  // const followersCount = followers.meta.result_count;
 
-  const tweets = await realUser.v2.search({
-    'tweet.fields': 'public_metrics',
-    query: `from:${user?.twitterName!}`,
-  });
-  let totalLikes = 0;
-  let totalComments = 0;
-  for await (const tweet of tweets) {
-    const likes = tweet.public_metrics!.like_count;
-    const comments = tweet.public_metrics!.reply_count;
-    totalComments += comments;
-    totalLikes += likes;
-  }
-  await prisma.growthData.create({
-    data: {
-      userId: Number(userId),
-      followers: followersCount,
-      likes: totalLikes,
-      comments: totalComments,
-      date: new Date(),
-    },
-  });
+  // const tweets = await realUser.v2.search({
+  //   'tweet.fields': 'public_metrics',
+  //   query: `from:${user?.twitterName!}`,
+  // });
+  // let totalLikes = 0;
+  // let totalComments = 0;
+  // for await (const tweet of tweets) {
+  //   const likes = tweet.public_metrics!.like_count;
+  //   const comments = tweet.public_metrics!.reply_count;
+  //   totalComments += comments;
+  //   totalLikes += likes;
+  // }
+  // await prisma.growthData.create({
+  //   data: {
+  //     userId: Number(userId),
+  //     followers: followersCount,
+  //     likes: totalLikes,
+  //     comments: totalComments,
+  //     date: new Date(),
+  //   },
+  // });
   res.redirect(process.env.FRONT_END_URL + '/dashboard/user-preferences');
 };
